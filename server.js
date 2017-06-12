7204656d9721b6c516eca9261b2e8317fd883892
Jay Chung <xjkit54780@gmail.com>
2017-06-12 11:11:27 +0800

    Fix data and path issue

--- server.js
+++ server.js
@@ -2,18 +2,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3001;
-
-const fakeData = require('./public/todos');
+const PORT = process.env.PORT || 3001;
 
 app.use('/', express.static(__dirname));
 
 app.get('/data.json', (req, res) => {
-  res.sendFile(fakeData);
+  res.sendFile(path.resolve(__dirname, 'public', 'todos.json'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, 'localhost', (err) => {
